a6d0cef4627de7eee3b9324a1b6c9f876b13a4d6
Menighin <joao.menighin@gmail.com>
2017-08-22 23:57:49 -0300

    Reading Treasury page + Fix with driver

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 RICO_LOGIN_URL = 'https://www.rico.com.vc/login?url=/dashboard/'
+RICO_TREASURY_PAGE = 'https://www.rico.com.vc/dashboard/tesouro-direto/'
+RICO_FIXED_INCOME_PAGE = 'https://www.rico.com.vc/dashboard/renda-fixa/'
 MAX_WAIT_TIME = 60
 
 def tryGetElement(driver, selector: str) -> object:
@@ -25,16 +27,19 @@ def get_actives_data(props: dict):
     password = props['password']
     webdriverPath = props['chromeWebdriver'] if 'chromeWebdriver' in props else None
 
-    driver = webdriver.Chrome(webdriverPath) if webdriverPath is not None else webdriver.Chrome()
-    driver.get(RICO_LOGIN_URL)
+    options = webdriver.ChromeOptions() 
+    options.add_argument("user-data-dir=C:\\Users\\Menighin\\AppData\\Local\\Google\\Chrome\\User Data") #Path to your chrome profile
+
+    driver = webdriver.Chrome(webdriverPath, chrome_options = options) if webdriverPath is not None else webdriver.Chrome(chrome_options = options)
+
     driver.implicitly_wait(MAX_WAIT_TIME) # Wait for element to appear in each call
+    driver.get(RICO_LOGIN_URL)
 
     form_user =  driver.find_element_by_name('loginController.usernameForm')
     input_username = form_user.find_element_by_xpath(".//input[@placeholder='Usuário']")
     input_username.send_keys(username)
     time.sleep(0.2)
     form_user.submit()
-
     form_password = driver.find_element_by_name('loginController.passwordForm')
     buttons = form_password.find_elements_by_tag_name('button')
 
@@ -48,7 +53,7 @@ def get_actives_data(props: dict):
     # Crawling the home page
     json = {}
     summary = tryGetElement(driver, '#home-step-menu-resumo-investimentos table')
-    driver.implicitly_wait(0)
+    driver.implicitly_wait(1)
     home_treasury = tryGetElement(driver, "div[summary-position-product='treasury'] #tableAllocatedValue")
     fixed_income = tryGetElement(driver, "div[summary-position-product='fixed-income'] #tableAllocatedValue")
     funds = tryGetElement(driver, "div[summary-position-product='funds'] #tableAllocatedValue")
@@ -61,6 +66,26 @@ def get_actives_data(props: dict):
     json['home']['fixedIncome'] = parser.parse_home_table(fixed_income.get_attribute('innerHTML'), parser.HomeTableType.ZERO_THREE)
     json['home']['funds'] = parser.parse_home_table(funds.get_attribute('innerHTML'), parser.HomeTableType.ZERO_ONE_TWO)
     
+    # Crawling the Treasury page
+    json['homeTreasury'] = []
+    driver.get(RICO_TREASURY_PAGE)
+
+    tbodies = driver.find_elements_by_css_selector('#tableAllocatedValue tbody')
+    i = 0
+    for tbody in tbodies: # Ignores first tbody
+        if i == 0:
+            i += 1
+            continue
+        json['homeTreasury'].append(parser.parse_treasury_item(tbody.get_attribute('innerHTML')))
+
+
+    # Crawling Fixed Income
+    driver.get(RICO_TREASURY_PAGE)
+
+    json['fixedIncome'] = parser.parse_fixed_income(driver.find_element_by_css_selector('#tableAllocatedValue tbody').get_attribute('innerHTML'))
+
+    print(json['fixedIncome'])
+
     driver.quit()
 
 
--- parsers.py
+++ parsers.py
@@ -19,11 +19,11 @@ def parse_home_table(html: str, tableType: HomeTableType) -> dict:
         tds = tr.find_all('td')
         applied = '-'
         if tableType == HomeTableType.ZERO_TWO_THREE:
-            name, *_, applied, actual = [ td.text.strip() for td in tr.find_all('td') ]
+            name, *_, applied, actual = [ td.text.strip() for td in tds ]
         elif tableType == HomeTableType.ZERO_ONE_TWO:
-            name, applied, actual, *_ = [ td.text.strip() for td in tr.find_all('td') ]
+            name, applied, actual, *_ = [ td.text.strip() for td in tds ]
         elif tableType == HomeTableType.ZERO_THREE:
-            name, _, _, actual, *_ = [ td.text.strip() for td in tr.find_all('td') ]
+            name, _, _, actual, *_ = [ td.text.strip() for td in tds ]
 
         if applied == '--':
             continue
@@ -33,5 +33,52 @@ def parse_home_table(html: str, tableType: HomeTableType) -> dict:
         else:
             json.append({ 'name': name, 'actual': brl_to_decimal(actual) })
     
-    print(json)
+    return json
+
+def parse_treasury_item(html: str):
+
+    dom = Soup(html, 'html.parser')
+    json = {}
+
+    i = 0
+    for tr in dom.find_all('tr')[:-1]:
+
+        if i == 1: # It is the headers for the products
+            i += 1
+            continue
+
+        tds = tr.find_all('td')
+
+        if i == 0: # It is the consolidated
+            name, _, _, applied, actual, *_ = [ td.text.strip() for td in tds ]
+
+            json = { 'name': name, 'applied': brl_to_decimal(applied), 'actual': brl_to_decimal(actual), 'products': [] }
+
+        else:
+            _, name, _, _, applied, actual, *_ = [ td.text.strip() for td in tds ]
+
+            json['products'].append({ 'name': name, 'applied': brl_to_decimal(applied), 'actual': brl_to_decimal(actual)})
+
+        i += 1
+
+    return json
+
+def parse_fixed_income(html: str):
+
+    dom = Soup(html, 'html.parser')
+    json = []
+
+    print(html)
+
+    for tr in dom.find_all('tr')[1:]:
+
+        print('----------------')
+        print(tr)
+
+        tds = tr.find_all('td')
+
+        name, _, _, _, applied, actual, *_ = [ td.text.strip() for td in tds ]
+
+        json.append({ 'name': name, 'applied': brl_to_decimal(applied), 'actual': brl_to_decimal(actual) })
+
     return json
