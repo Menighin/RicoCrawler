c159f254c4e0483b686989355b018eda37660b20
Menighin <joao.menighin@gmail.com>
2017-08-24 21:44:22 -0300

    Pages built

--- html/app.js
+++ html/app.js
@@ -8,7 +8,7 @@ function renderChart(chartId, dataKey, period) {
     
     let chart = {
         title: {
-            text: 'OIOIOI'
+            text: $('#' + chartId + '-title').val()
         },
         yAxis: {
             title: {
@@ -52,7 +52,8 @@ function renderChart(chartId, dataKey, period) {
                 }
             });
         } else { // Get one date by month
-            let month = new Date(d.date[d.date.length - 1]).getMonth() + 1;
+            let date = new Date(d.date[d.date.length - 1])
+            let month = date.getFullYear() + date.getMonth() + 1;
 
             CONFIG.PROPS.forEach((p) => {
                 if (d.hasOwnProperty(p.key)) {
@@ -62,7 +63,7 @@ function renderChart(chartId, dataKey, period) {
                     for (let j = d[p.key].length - 1; j >= 0; j--) {
 
                         let currentDate = new Date(d.date[j]);
-                        if (currentDate.getMonth() < month) {
+                        if (currentDate.getFullYear() + currentDate.getMonth() < month) {
                             actualData.push([ currentDate.getTime(), parseFloat(d[p.key][j]) ]);
                             month = currentDate.getMonth();
                         }
@@ -78,17 +79,23 @@ function renderChart(chartId, dataKey, period) {
     Highcharts.chart(chartId, chart);
 }
 
+function loadPage(li, key) {
+    let template = $('#chart-template').html();
+    
+    let chartModels = CONFIG.PAGES[key];
+
+    $('#content').html('');
+
+    chartModels.forEach((model) => {
+        let rendered = Mustache.render(template, model);
+        $('#content').append(rendered)
+        $('#content').append('<hr />')
+        renderChart(model.chartId, model.dataKey, 'weekly');
+    });
+}
 
 $(document).ready(() => {
-    let template = $('#chart-template').html();
 
-    let chartModel = {
-        chartId: 'summary',
-        dataKey: 'home.summary'
-    };
-
-    let rendered = Mustache.render(template, chartModel);
-
-    $('#content').html(rendered)
+    loadPage(null, 'home');
 
 });
--- html/config.js
+++ html/config.js
@@ -2,5 +2,19 @@ var CONFIG = {
     PROPS: [
         { key: 'actual', label: '(Actual)' },
         { key: 'applied', label: '(Applied)' }
-    ]
+    ],
+    PAGES: {
+        home: [
+            { chartId: 'summary', dataKey: 'home.summary', title: 'Sumário Geral' },
+            { chartId: 'home-treasury', dataKey: 'home.homeTreasury', title: 'Sumário Tesouro Direto' },
+            { chartId: 'fixed-income', dataKey: 'home.fixedIncome', title: 'Sumário Renda Fixa' },
+            { chartId: 'funds', dataKey: 'home.funds', title: 'Sumário Fundos de Investimento' }
+        ],
+        treasury: [
+            { chartId: 'home-treasury', dataKey: 'homeTreasury', title: 'Tesouro Direto' }
+        ],
+        fixedIncome: [
+            { chartId: 'fixed-income', dataKey: 'fixedIncome', title: 'Renda Fixa' }
+        ]
+    }
 }
